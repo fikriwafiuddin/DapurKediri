8f9d6e63104220bf1129210b727117ebb0ec4328
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-11-04 06:14:03 +0700

    feat: Added jwt token validity period to 7 days

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"
 
-export const generateToken = (payload, expiresIn = "1h") => {
+export const generateToken = (payload, expiresIn = "7d") => {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn })
 }
 
